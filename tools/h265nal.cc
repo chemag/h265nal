025fc72edadbd63ec3da5c06608c7fd40f2af65b
Chema Gonzalez <chemag@fb.com>
2021-12-30 14:37:57 -0800

    h265nal: tools: lint h265nal.cc

--- tools/h265nal.cc
+++ tools/h265nal.cc
@@ -31,15 +31,18 @@ typedef struct arg_options {
 } arg_options;
 
 // default option values
-arg_options DEFAULTS{.debug = 0,
-                     .as_one_line = true,
-                     .add_offset = false,
-                     .add_length = false,
-                     .add_parsed_length = false,
-                     .add_checksum = false,
-                     .add_contents = false,
-                     .infile = nullptr,
-                     .outfile = nullptr};
+arg_options DEFAULTS{
+    .debug = 0,
+    .as_one_line = true,
+    .add_offset = false,
+    .add_length = false,
+    .add_parsed_length = false,
+    .add_checksum = false,
+    .add_contents = false,
+    .infile = nullptr,
+    .outfile = nullptr,
+};
+
 void usage(char *name) {
   fprintf(stderr, "usage: %s [options]\n", name);
   fprintf(stderr, "where options are:\n");
@@ -202,6 +205,7 @@ arg_options *parse_args(int argc, char **argv) {
         printf("version: %s\n", PROJECT_VER);
         exit(0);
         break;
+
       case HELP_OPTION:
       case 'h':
         usage(argv[0]);
