4bbaf44778a3089f07a0ed6e247ebd91c972cb03
Chema Gonzalez <chemag@fb.com>
2022-10-27 08:47:33 -0700

    h265nal: add limits for vui->log2_max_mv_length_vertical

--- src/h265_vui_parameters_parser.cc
+++ src/h265_vui_parameters_parser.cc
@@ -386,6 +386,18 @@ H265VuiParametersParser::ParseVuiParameters(
     if (!bit_buffer->ReadExponentialGolomb(vui->log2_max_mv_length_vertical)) {
       return nullptr;
     }
+    if (vui->log2_max_mv_length_vertical < kLog2MaxMvLengthVerticalMin ||
+        vui->log2_max_mv_length_vertical > kLog2MaxMvLengthVerticalMax) {
+#ifdef FPRINT_ERRORS
+      fprintf(stderr,
+              "invalid log2_max_mv_length_vertical: %" PRIu32
+              " not in range "
+              "[%" PRIu32 ", %" PRIu32 "]\n",
+              vui->log2_max_mv_length_vertical, kLog2MaxMvLengthVerticalMin,
+              kLog2MaxMvLengthVerticalMax);
+#endif  // FPRINT_ERRORS
+      return nullptr;
+    }
   }
 
   return vui;
