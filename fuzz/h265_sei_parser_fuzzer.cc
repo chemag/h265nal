eeda692d86beb743c2fcc2388d77c4c657b66fc3
Chema Gonzalez <chemag@meta.com>
2023-10-02 11:47:45 -0700

    h265nal: add h265_sei_parser fuzzer

---
+++ fuzz/h265_sei_parser_fuzzer.cc
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) Facebook, Inc. and its affiliates.
+ */
+
+// This file was auto-generated using fuzz/converter.py from
+// h265_sei_parser_unittest.cc.
+// Do not edit directly.
+
+#include "h265_sei_parser.h"
+#include "h265_common.h"
+#include "rtc_base/arraysize.h"
+#include "rtc_base/bit_buffer.h"
+
+
+// libfuzzer infra to test the fuzz target
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  {
+  auto sei_message = h265nal::H265SeiMessageParser::ParseSei(data, size);
+  }
+  return 0;
+}
