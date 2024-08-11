4853b544d260af3a97544abe9f55ca09a42c5a98
Chema Gonzalez <chemag@meta.com>
2024-08-11 11:07:29 -0700

    h265nal: add HEVCConfigurationBox fuzzer
    Tested:
    ```
    $ ../build/fuzz/h265_configuration_box_parser_fuzzer -runs=10000000
    ...
    non-existent SPS id: 0
     #10000000 DONE   cov: 241 ft: 925 corp: 106/71Kb lim: 4096 exec/s: 8019 rss: 561Mb
     ###### Recommended dictionary. ######
    "\000\000" # Uses: 95974
    "\377\377\377\377" # Uses: 94888
    "\000\000\000\000\000\000\000\000" # Uses: 93309
    "\001\000" # Uses: 89808
    "\377\377\377\377\377\377\377\377" # Uses: 87782
    "\"gmO\301Z)\347" # Uses: 83401
    "\000\000\000\000" # Uses: 72964
    "\001\000\000\000\000\000\000\000" # Uses: 64294
    "\003\000" # Uses: 53815
    "N10__cxxabiv120__si_class_type_in" # Uses: 47681
    "N10__cxxabiv" # Uses: 39866
    "\250\317\306/F+\315\321" # Uses: 39433
    "\001\000\000\000\001\000\000\000" # Uses: 35279
    "\001\000\000\000" # Uses: 34196
     ###### End of recommended dictionary. ######
    Done 10000000 runs in 1247 second(s)
    ```

---
+++ fuzz/h265_configuration_box_parser_fuzzer.cc
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) Facebook, Inc. and its affiliates.
+ */
+
+// This file was auto-generated using fuzz/converter.py from
+// h265_configuration_box_parser_unittest.cc.
+// Do not edit directly.
+
+#include "h265_configuration_box_parser.h"
+#include "h265_common.h"
+#include "rtc_base/arraysize.h"
+#include "rtc_base/bit_buffer.h"
+
+
+// libfuzzer infra to test the fuzz target
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  {
+  h265nal::H265BitstreamParserState bitstream_parser_state;
+  h265nal::ParsingOptions parsing_options;
+  auto configuration_box = h265nal::H265ConfigurationBoxParser::ParseConfigurationBox(
+      data, size, &bitstream_parser_state, parsing_options);
+  }
+  return 0;
+}
