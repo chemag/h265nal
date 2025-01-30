e8f673cabcac34dc10061c2073a1c9c2eb2468fc
Chema Gonzalez <chemag@meta.com>
2025-01-29 16:31:00 -0800

    h265nal: clean-up file descriptor

--- tools/h265nal.cc
+++ tools/h265nal.cc
@@ -392,11 +392,13 @@ int main(int argc, char **argv) {
 
   // 4.1. get outfile file descriptor
   FILE *outfp;
+  bool must_close_fp = false;
   if (options->outfile == nullptr ||
       (strlen(options->outfile) == 1 && options->outfile[0] == '-')) {
     // use stdout
     outfp = stdout;
   } else {
+    must_close_fp = true;
     outfp = fopen(options->outfile, "wb");
     if (outfp == nullptr) {
       // did not work
@@ -430,6 +432,11 @@ int main(int argc, char **argv) {
     }
   }
 #endif  // FDUMP_DEFINE
+
+  // clean-up FD
+  if (must_close_fp) {
+    fclose(outfp);
+  }
 
   return 0;
 }
