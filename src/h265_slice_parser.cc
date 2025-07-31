547b3526faf9a944dded26de020e87f603fa0811
Jian Zhou <zhoujian@meta.com>
2025-07-31 15:15:23 -0700

    h265nal: replace int types include

--- src/h265_slice_parser.cc
+++ src/h265_slice_parser.cc
@@ -4,8 +4,8 @@
 
 #include "h265_slice_parser.h"
 
-#define __STDC_FORMAT_MACROS
-#include <inttypes.h>
+
+#include <cinttypes>
 #include <stdio.h>
 
 #include <cmath>
--- src/h265_sps_multilayer_extension_parser.cc
+++ src/h265_sps_multilayer_extension_parser.cc
@@ -4,8 +4,8 @@
 
 #include "h265_sps_multilayer_extension_parser.h"
 
-#define __STDC_FORMAT_MACROS
-#include <inttypes.h>
+
+#include <cinttypes>
 #include <stdio.h>
 
 #include <cstdint>
